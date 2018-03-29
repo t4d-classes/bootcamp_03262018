7d5f3deb7a31b5fb734749b3a38cf82d869d126a
Eric W. Greene <eric@ericwgreene.com>
2018-03-29 09:48:55 -0700

    added code demos

---
+++ src/components/ColorTool.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export class ColorTool extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <header>
+         <h1>Color Tool</h1>
+        </header>
+      </div>
+    );
+  }
+}
